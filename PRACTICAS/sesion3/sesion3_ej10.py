6528c9496935826d456b115bdda297fda8deb894
javigp2002 <javigp2002@gmail.com>
2023-10-25 13:15:31 +0200

    Fixed: Fin ejercicio es_inversa con otra funcion

--- PRACTICAS/sesion3/sesion3_ej10.py
+++ PRACTICAS/sesion3/sesion3_ej10.py
@@ -6,11 +6,31 @@
 def es_inversa(palabra1, palabra2):
     return palabra1 == palabra2[::-1]
 
+def es_inversa_v2(palabra1, palabra2):
+    if (len(palabra1) != len(palabra2)):
+        return False
+    else:
+        longPalabra=len(palabra1)
+        i=0
+        while i < longPalabra:
+            if palabra1[i] == palabra2[longPalabra-i-1]:
+                i += 1
+            else:
+                return False
+        return True
+
+
 palabra1 = input("Introduce una palabra: ")
 palabra2 = input("Introduce otra palabra: ")
 
 print("Primera version")
 if es_inversa(palabra1, palabra2):
+    print("La palabra", palabra1, "es inversa de", palabra2)
+else:
+    print("La palabra", palabra1, "no es inversa de", palabra2)
+
+print("Segunda version")
+if es_inversa_v2(palabra1, palabra2):
     print("La palabra", palabra1, "es inversa de", palabra2)
 else:
     print("La palabra", palabra1, "no es inversa de", palabra2)
